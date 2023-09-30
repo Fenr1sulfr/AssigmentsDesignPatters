f8ff25db9c4c0f347f04eec28d7c243a48294435
Fenr1sulfr <moldakhmetov00@bk.ru>
2023-09-30 23:36:36 +0600

    123

--- main.go
+++ main.go
@@ -1,70 +1,54 @@
 package gotour
 
-import (
-	"fmt"
-)
+import "fmt"
 
+type Gamer interface {
+	accept(g *Game) error
+}
 type Game struct {
 	name    string
+	id      int
 	players []Player
-	isStart bool
 }
-type Gamer interface {
-	accept(*Game)
-}
-
 type Player struct {
-	name   string
-	id     int
-	choice bool
-}
-type LeadPlayer struct {
-	name             string
-	id               int
-	thisChoiceMatter bool
+	name string
+	id   int
+	role string
 }
 
-func (p *Player) accept(g *Game) {
-	if p.choice {
+func (p *Player) accept(g *Game) error {
+	if p.role == "Support" {
 		g.players = append(g.players, *p)
 	}
+	return nil
 }
-func (l *LeadPlayer) accept(g *Game) {
-	if l.thisChoiceMatter {
-		for i := 0; i < len(g.players); i++ {
-			g.players[i].choice = true
-		}
+
+func (g *Game) notifyAllPlayers() {
+	for i, v := range g.players {
+		fmt.Println("%a , prepare for battle! Your role %b Full info:%c", g.players[i].name, g.players[i].role, v)
 	}
 }
-
-func Start(g Gamer, gm *Game) {
-	switch g.(type) {
-	case *LeadPlayer:
-		fmt.Println("Your party leader start the game")
-
-	case *Player:
-		for i := 0; i < len(gm.players); i++ {
-			if !gm.players[i].choice {
-				fmt.Println("Somedoby decline")
-			}
-		}
-	default:
-		fmt.Println("Somebody didn't accept the game ")
-	}
+func startTheGame(g Gamer, gm *Game) {
+	g.accept(gm)
+	gm.notifyAllPlayers()
 }
-
 func main() {
-	p1 := Player{name: "1000-7?", id: 1, choice: false}
-	p2 := Player{name: "all muted", id: 2, choice: true}
-	lp := LeadPlayer{name: "mid or feed", id: 3, thisChoiceMatter: true}
-
-	game := Game{
-		name:    "Dota 2",
-		players: []Player{},
-		isStart: false,
+	p1 := Player{
+		name: "all muted",
+		id:   1,
+		role: "Support",
 	}
-	p1.accept(&game)
-	p2.accept(&game)
-	lp.accept(&game)
-	Start(&p1, &game)
+
+	p2 := Player{
+		name: "mid or feed",
+		id:   1,
+		role: "Support",
+	}
+	g := Game{
+		name:    "Dota 2",
+		id:      1,
+		players: []Player{},
+	}
+	startTheGame(&p1, &g)
+	startTheGame(&p2, &g)
 }
